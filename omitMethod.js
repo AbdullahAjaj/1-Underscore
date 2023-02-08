2303630755db403f2af6682b0dcdfa87c80474fb
AbdullahAjaj <abdollahmjajaj@gmail.com>
2023-02-08 20:51:27 +0200

    omit method v2

--- omitMethod.js
+++ omitMethod.js
@@ -1,15 +1,30 @@
 let obj = {name: 'moep', age: 50, userid: 'oe1'}
 
-function omit(obj, propOrFunction){
+function omit1(obj, propOrFunction){
     let propName = 
         typeof propOrFunction === "function" 
         ? propOrFunction
         : item => propOrFunction === item
 
+    // this solution is not safe
     for(let item in obj){
         if(propName(item)) delete obj[item]
     }
     return obj
 }
 
-console.log(omit(obj, "name"))
+function omit2(obj, propOrFunction){
+    let propName = 
+        typeof propOrFunction === "function" 
+        ? propOrFunction
+        : item => propOrFunction === item
+
+    // safe solution
+    let result = {} ;
+    for(let key in obj){
+        if(!propName(key)) result[key]= obj[key];
+    }
+    return result
+}
+    
+console.log(omit2(obj, "name"))
