d1ce39fede81dbfb3374b6909ec388e9e6c67eec
AbdullahAjaj <abdollahmjajaj@gmail.com>
2023-02-08 20:29:09 +0200

    chunk method v2

--- chunkMethod.js
+++ chunkMethod.js
@@ -1,6 +1,7 @@
 let arr = [2,4,2,5,3,8,4,7,4]
 
-function chunk(arr, num){
+// chunk method using shift
+function chunk1(arr, num){
     let newArr = []
     let index = 0
     while(arr.length > 0){
@@ -14,5 +15,22 @@ function chunk(arr, num){
     return newArr
 }
 
+// chunk method using slice
+function chunk2(arr, num){
+    const newArr = [];
+    for(let i=0; i<arr.length/num; i++){
+        newArr.push(arr.slice(i*num, (i+1)*num));
+    }
+    return newArr;
+}
 
-console.log(chunk(arr, 4))
+// chunk method using reduce
+function chunk3(arr, num){
+    return Array.from({length : arr.length/num}).reduce((acc,_,i)=>{
+        return [...acc, arr.slice(i*num, (i+1)*num)];
+    },[]);   
+}
+
+
+
+console.log(chunk1(arr, 4))
