241deac5c929576626f8c3c894cf0a92a53a2e3e
AbdullahAjaj <abdollahmjajaj@gmail.com>
2023-02-09 10:03:25 +0200

    isEqual method v2

--- isEqualMethod.js
+++ isEqualMethod.js
@@ -10,20 +10,44 @@ let person2 = {
     avg: 95,
 };
 
+let person3 = {
+    name: "ahmad",
+    avg: 95,
+    location: {
+        city: "nablus",
+        id: 33
+    }
+};
+
+let person4 = {
+    name: "ahmad",
+    avg: 95,
+    location: {
+        city: "nablus",
+        id: 33
+    }
+};
+
 // function isEqual(obj1, obj2){
 //     return JSON.stringify(obj1) === JSON.stringify(obj2)
 // }
 
 function isEqual(obj1, obj2){
-    let maxLength = Math.max(Object.keys(obj1).length, Object.keys(obj2).length)
+    let flag = true
+    if(Object.keys(obj1).length != Object.keys(obj2).length) return false
 
     for(let item in obj1){
-        if(obj1[item] !== obj2[item]) return false
-        maxLength--;
+        if(typeof obj1[item] === "object"){
+            Object.entries(obj1[item]).forEach(([key, value]) => {
+                if(obj1[item][key] !== obj2[item][key]){ 
+                    flag = false
+                    return
+                }
+            });
+        } 
+        else if(obj1.item !== obj2.item) flag = false
     }
-
-    if(maxLength != 0) return false
-    return true
+    return flag
 }
 
-console.log(isEqual(person1, person2))
+console.log(isEqual(person3, person4))
